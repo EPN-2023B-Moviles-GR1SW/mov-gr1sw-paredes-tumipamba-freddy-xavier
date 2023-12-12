e5e8ad18de24d73e3e8d8cad984bb0e9e83a431b
Xavier <freddy.paredes@epn.edu.ec>
2023-12-11 23:54:49 -0500

    Trabajo en clase

--- Android/app/src/main/java/com/example/gr1_sw_fxpt/MainActivity.kt
+++ Android/app/src/main/java/com/example/gr1_sw_fxpt/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gr1_sw_fxpt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,32 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gr1_sw_fxpt.ui.theme.Gr1swfxptTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Gr1swfxptTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Gr1swfxptTheme {
-        Greeting("Android")
-    }
-}
