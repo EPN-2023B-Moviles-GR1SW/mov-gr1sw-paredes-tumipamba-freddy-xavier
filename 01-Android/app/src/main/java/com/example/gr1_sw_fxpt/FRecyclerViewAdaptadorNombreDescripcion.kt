953291b7c028d9c9617f0e6c651f50ad4570b07e
Xavier <freddy.paredes@epn.edu.ec>
2024-01-21 21:42:24 -0500

    Proyecto primer bimestre

--- 01-Android/app/src/main/java/com/example/gr1_sw_fxpt/FRecyclerViewAdaptadorNombreDescripcion.kt
+++ 01-Android/app/src/main/java/com/example/gr1_sw_fxpt/FRecyclerViewAdaptadorNombreDescripcion.kt
@@ -2,6 +2,8 @@ package com.example.gr1_sw_fxpt
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class FRecyclerViewAdaptadorNombreDescripcion(
@@ -14,6 +16,17 @@ class FRecyclerViewAdaptadorNombreDescripcion(
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(
         view
     ) {
+        val nombreTextView: TextView
+        val descripcionTextView: TextView
+        val likesTextView: TextView
+        val accionButton: Button
+        var numeroLikes = 0
+        init {
+            nombreTextView = view.findViewById(R.id.tv_nombre)
+            descripcionTextView = view.findViewById(R.id.tv_descripcion)
+            likesTextView = view.findViewById(R.id.tv_likes)
+            accionButton = view.findViewById(R.id.btn_dar_like)
+            accionButton.setOnClickListener { anadirLike() }
     }
 
     override fun onCreateViewHolder(
